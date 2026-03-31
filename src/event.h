d69bd203406ec6302f7da95c22984b1b0a82cfeb
siddhivn <b24bb1036@iitj.ac.in>
2026-04-01 00:11:26 +0530

    Create event.h

---
+++ src/event.h
@@ -0,0 +1,19 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <functional>
+#include <string>
+using namespace std;
+
+struct Event {
+    int time;                  // event time
+    string type;              // event type (e.g., PACKET_SEND)
+    function<void()> callback; // function to execute
+
+    // for priority queue (min-heap)
+    bool operator>(const Event& other) const {
+        return time > other.time;
+    }
+};
+
+#endif
