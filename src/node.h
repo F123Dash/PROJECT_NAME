95d4c09a136387db34dd78f69fb757981cb6c9c2
himaniii <b24me1033@iitj.ac.in>
2026-04-03 16:49:16 +0530

    Create node.h

---
+++ src/node.h
@@ -0,0 +1,34 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <map>
+#include <queue>
+#include <vector>
+#include <string>
+#include "packet.h"
+using namespace std;
+
+struct Node {
+    int id;                          // unique node ID
+    map<int, int> routing_table;     // destination → next hop node ID
+    queue<Packet> pkt_queue;         // packets waiting to be sent
+    vector<int> interfaces;          // connected link IDs
+
+    // constructor
+    Node(int node_id);
+
+    void add_route(int destination, int next_hop);
+
+    void add_interface(int link_id);     // add a link interface
+
+    int get_next_hop(int destination) const; // (-1 if no route)
+
+    void send_packet(Packet& pkt); //puts packet in queue, records hop
+
+    void receive_packet(Packet& pkt); //processes incoming packet
+
+    // debug
+    void print_info() const;
+};
+
+#endif
